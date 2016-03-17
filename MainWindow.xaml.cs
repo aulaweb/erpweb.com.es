2ced87ba6132bc52c4848141efe0650d80c9213b
Juanjo Pedraza <aulaweb.com.es@gmail.com>
2016-03-17 20:45:32 +0100

    Version1

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace erpweb.com.es
+{
+    /// <summary>
+    /// Lógica de interacción para MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void MnSalir_Click(object sender, RoutedEventArgs e)
+        {
+            var resultado = MessageBox.Show("¿Estás seguro que quieres salir?",
+                "salir del programa",MessageBoxButton.YesNo,MessageBoxImage.Warning);
+            if (resultado==MessageBoxResult.Yes)
+            {
+                this.Close();
+            }
+            
+        }
+    }
+}
